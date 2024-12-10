d4148aab48da0ecf83bca6e83b6e2063d5b60c49
Vedashree-20 <vedashreeshere@gmail.com>
2024-12-09 22:04:39 -0500

    Adding user rating to the popular movie list

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ function displayMovieGenre(genreCode) {
       .catch((err) => console.error(err));
   });
 }
+
+//Get the average user score in percentage
 function calculateAveragePercentage(average) {
   let averagePercentage = (average / 10) * 100;
   averagePercentage = Math.floor(averagePercentage);
@@ -44,6 +46,13 @@ function calculateAveragePercentage(average) {
   //return averagePercentage;
 }
 
+//Get year from the release date string
+function getYear(date) {
+  let dateString = new Date(date);
+  let year = dateString.getFullYear();
+  return year;
+}
+
 //Get the hero section background image
 
 function showMovieDetails(data) {
@@ -51,8 +60,7 @@ function showMovieDetails(data) {
   let movieName = data[0].title;
   let overview = data[0].overview;
   let releaseDate = data[0].release_date;
-  let dateString = new Date(releaseDate);
-  let year = dateString.getFullYear();
+  let year = getYear(releaseDate);
   let voterAverage = data[0].vote_average;
   let averageVoterScore = calculateAveragePercentage(voterAverage);
   let movieGenreIds = data[0].genre_ids;
@@ -76,7 +84,7 @@ function showMovieDetails(data) {
     releaseDateDiv.innerHTML = year;
   });
 }
-
+//Display background image for movie
 function displayBackground(response) {
   console.log(response);
   console.log(response.data.results[0].backdrop_path);
@@ -92,6 +100,7 @@ function displayBackground(response) {
   showMovieDetails(response.data.results);
 }
 
+//Display popular movie list
 function displayPopularMovie(response) {
   let popularMovieList = "";
 
@@ -100,15 +109,21 @@ function displayPopularMovie(response) {
     let posterUrl = `https://image.tmdb.org/t/p/original${posterPath}`;
     let movieName = response.data.results[i].title;
     let releaseDate = response.data.results[i].release_date;
+    let yearOfRelease = getYear(releaseDate);
+    let averageVoterScore = response.data.results[i].vote_average;
+    let averagePercentage = calculateAveragePercentage(averageVoterScore);
     popularMovieList += `<div class=movie-card>
-        <div class="image"><img src="${posterUrl}" alt="${movieName}" width="250" height="350" object-fit= "cover";></div>
+        <div class="popularmovie-image"><img src="${posterUrl}" alt="${movieName}" width="250" height="350" object-fit= "cover";></div>
+        <div class="overlay"></div>
+        <div class="popularmovie-rating">
+            <span class="score ${averagePercentage[1]}" id="scoreColor"> ${averagePercentage[0]}</span>
+        </div>
         <div class="movie-name"> ${movieName}</div>
-        <div class="release-date">${releaseDate}</div>
+        <div class="release-date">${yearOfRelease}</div>
         </div>`;
   }
   let popularMovieDiv = document.querySelector(".movie-list");
   popularMovieDiv.innerHTML = popularMovieList;
-  console.log(popularMovieList);
 }
 
 //API call to get the display picture from trending movies
@@ -144,4 +159,27 @@ const options2 = {
 axios
   .request(options2)
   .then(displayPopularMovie)
+  .catch((err) => console.error(err));
+
+const options3 = {
+  method: "GET",
+  url: "https://api.themoviedb.org/3/discover/movie",
+  params: {
+    include_adult: "false",
+    include_video: "false",
+    language: "en-US",
+    sort_by: "popularity.desc",
+    "vote_average.gte": "7",
+    with_genres: "28",
+  },
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjllZjI1MTczNWRkY2VmYjhmNjhlMmFlOTgwZmJlMyIsIm5iZiI6MTczMTQyNTA1NS40NDQ0MDE1LCJzdWIiOiI2NzMyMjQ2ZWE3ZmRjODYwODc4OGIwZDUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.V3sZOi3jIN-mFIgTuZ2vkK4uBGQ76SN_N18VnYv-BTU",
+  },
+};
+
+axios
+  .request(options3)
+  .then((res) => console.log(res.data))
   .catch((err) => console.error(err));
